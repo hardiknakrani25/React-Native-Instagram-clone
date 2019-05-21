0651b6a360f1c084e20aa2ea6ef495a06deae074
Hardik <hdnakarani@gmail.com>
2019-05-21 19:41:58 +0530

    design header of activity tab

--- Components/AppTabNavigator/LikesTab.js
+++ Components/AppTabNavigator/LikesTab.js
@@ -1,30 +1,56 @@
-import React, {Component} from 'react';
-import {StyleSheet, View, Text} from 'react-native';
-import {Icon} from "native-base";
+import React, { Component } from "react";
+import { StyleSheet, View, Text, Platform } from "react-native";
+import {
+  Header,
+  Icon,
+  Container,
+  Content,
+  Thumbnail,
+  Left,
+  Right,
+  Body
+} from "native-base";
 
 export default class LikesTab extends Component {
+  static navigationOptions = {
+    tabBarIcon: ({ tintColor }) => (
+      <Icon name="ios-heart" style={{ color: tintColor }} />
+    )
+  };
 
-
-    static navigationOptions = {
-        tabBarIcon: ({tintColor}) => (
-            <Icon name="ios-heart" style={{color: tintColor}}/>
-        )
-    }
-
-
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>LikesTab </Text>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <Container style={styles.container}>
+        <Header style={[styles.androidHeader]}>
+          <Left />
+          <Body style={[styles.androidHeaderTitle]}>
+            <Text style={{ fontWeight: "bold", fontSize: 16 }}>Activity</Text>
+          </Body>
+          <Right />
+        </Header>
+        <Content />
+      </Container>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: "white"
+  },
+  androidHeader: {
+    ...Platform.select({
+      android: {
+        backgroundColor: "white"
+      }
+    })
+  },
+  androidHeaderTitle: {
+    ...Platform.select({
+      android: {
+        alignItems: "flex-end"
+      }
+    })
+  }
+});
