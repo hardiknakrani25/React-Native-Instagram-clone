f9c0548fad54047f885dd569cb8a05cb4b2fa62d
Hardik <hdnakarani@gmail.com>
2019-05-21 17:31:51 +0530

    searchbar design added

--- Components/AppTabNavigator/SearchTab.js
+++ Components/AppTabNavigator/SearchTab.js
@@ -1,30 +1,49 @@
-import React, {Component} from 'react';
-import {StyleSheet, View, Text} from 'react-native';
-import {Icon} from "native-base";
+import React, { Component } from "react";
+import { SafeAreaView, StyleSheet, View, Text } from "react-native";
+import { Icon } from "native-base";
+import { SearchBar } from "react-native-elements";
 
 export default class SearchTab extends Component {
+  state = {
+    search: ""
+  };
 
+  static navigationOptions = {
+    tabBarIcon: ({ tintColor }) => (
+      <Icon name="ios-search" style={{ color: tintColor }} />
+    )
+  };
 
-    static navigationOptions = {
-        tabBarIcon: ({tintColor}) => (
-            <Icon name="ios-search" style={{color: tintColor}}/>
-        )
-    }
+  updateSearch = search => {
+    this.setState({ search });
+  };
 
-
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>SearchTab </Text>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={{ marginRight: 15, marginLeft: 15 }}>
+          <SearchBar
+            lightTheme
+            round
+            containerStyle={{
+              backgroundColor: "#ffffff",
+              borderTopWidth: 0,
+              borderBottomWidth: 0
+            }}
+            inputContainerStyle={{ backgroundColor: "#E5E4EB" }}
+            showLoading
+            placeholder="Search"
+            onChangeText={this.updateSearch}
+            value={this.state.search}
+          />
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-})
+  container: {
+    flex: 1
+  }
+});
