8e1ddc2a016186afba1b110895c21d8d6ec614c6
Hardik <hdnakarani@gmail.com>
2019-05-22 15:33:06 +0530

    add following activity UI

--- Components/AppTabNavigator/LikesTab.js
+++ Components/AppTabNavigator/LikesTab.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Text, Platform, FlatList } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Platform,
+  FlatList,
+  Image
+} from "react-native";
 import {
   Header,
   Icon,
@@ -87,6 +94,64 @@ const list = [
     subtitle: "mention you in comment"
   }
 ];
+
+const followingList = [
+  {
+    name: "Chris Jackson",
+    avatar_url:
+      "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
+    subtitle: "liked photos",
+    time: "12m",
+    image_url1:
+      "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg"
+  },
+  {
+    name: "Amy Farha",
+    avatar_url:
+      "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg",
+    subtitle: "liked photos",
+    time: "17m",
+    image_url1:
+      "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg"
+  },
+  {
+    name: "Chris Jackson",
+    avatar_url:
+      "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
+    subtitle: "liked photos",
+    time: "48m",
+    image_url1:
+      "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg"
+  },
+  {
+    name: "Amy Farha",
+    avatar_url:
+      "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg",
+    subtitle: "liked photos",
+    time: "1h",
+    image_url1:
+      "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg"
+  },
+  {
+    name: "Chris Jackson",
+    avatar_url:
+      "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
+    subtitle: "liked photos",
+    time: "12h",
+    image_url1:
+      "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg"
+  },
+  {
+    name: "Amy Farha",
+    avatar_url:
+      "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg",
+    subtitle: "liked photos",
+    time: "3d",
+    image_url1:
+      "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg"
+  }
+];
+
 export default class LikesTab extends Component {
   constructor(props) {
     super();
@@ -120,6 +185,43 @@ export default class LikesTab extends Component {
     />
   );
 
+  followingListRender = ({ item }) => (
+    <ListItem
+      leftElement={<Text>{item.time}</Text>}
+      title={item.name}
+      topDivider
+      subtitleStyle={{ color: "#727272" }}
+      subtitle={item.subtitle}
+      bottomDivider
+      leftAvatar={{ source: { uri: item.avatar_url } }}
+      rightElement={
+        <Image
+          style={{ height: 50, width: 50 }}
+          source={{ uri: item.image_url1 }}
+        />
+      }
+    />
+  );
+
+  renderSelectedList() {
+    if (this.state.selectedIndex == 1) {
+      return (
+        <FlatList
+          keyExtractor={this.keyExtractor}
+          data={list}
+          renderItem={this.renderItem}
+        />
+      );
+    }
+    return (
+      <FlatList
+        keyExtractor={this.keyExtractor}
+        data={followingList}
+        renderItem={this.followingListRender}
+      />
+    );
+  }
+
   render() {
     const buttons = ["Following", "You"];
     const { selectedIndex } = this.state;
@@ -141,13 +243,7 @@ export default class LikesTab extends Component {
             selectedButtonStyle={{ backgroundColor: "black" }}
             containerStyle={{ height: 30 }}
           />
-          <View>
-            <FlatList
-              keyExtractor={this.keyExtractor}
-              data={list}
-              renderItem={this.renderItem}
-            />
-          </View>
+          <View>{this.renderSelectedList()}</View>
         </View>
 
         <Content />
